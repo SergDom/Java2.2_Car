ae346856751a183b719d018b7a79e3293ee29e8c
sergeydomanov <nomad-labs@yandex.ru>
2022-09-23 20:32:17 +0300

    new

--- src/Car.java
+++ src/Car.java
@@ -6,23 +6,43 @@ public class Car {
     int productionYear;
     String country;
 
-    public Car (String brand, String model, int productionYear,  String country, String color, double engine){
-        this.brand=brand;
-        this.model=model;
-        this.productionYear=productionYear;
-        this.country=country;
-        this.color=color;
-        this.engine=engine;
+    public Car(String brand, String model, int productionYear, String country, String color, double engine) {
+        if (brand == null){
+            this.brand = null; }
+        else { this.brand = brand; }
+
+        if (model == null){
+            this.model = null; }
+        else { this.model = model; }
+
+        if (productionYear == 0){
+            this.productionYear = 2000; }
+        else { this.productionYear = productionYear; }
+
+        if (country == null){
+            this.country = null; }
+        else { this.country = country; }
+
+        if (color == null){
+            this.color = "белого"; }
+        else { this.color = color; }
+
+        if (engine == 0) {
+            this.engine = 1.7;
+        } else {
+            this.engine = engine;
+        }
+
     }
 
     @Override
     public String toString() {
-        return  brand + " " + model + " " + productionYear + " года выпуска, " + "сборка в " + country + ", " + color + " цвета" +
+        return brand + " " + model + " " + productionYear + " года выпуска, " + "сборка в " + country + ", " + color + " цвета" +
                 " ,обЪем двигателя " + engine + " литра.";
     }
 
     public static void main(String[] args) {
-Car one = new Car("Lada", "Grande", 2015, "Россия", "желтого", 1.7);
+        Car one = new Car(null, "Grande", 2015, "Россия", null, 0);
 
 
         System.out.println(one);
