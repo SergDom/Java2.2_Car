eb4a7d2bbf3e78f594edf1768c9592d688bb957a
sergeydomanov <nomad-labs@yandex.ru>
2022-09-24 13:11:03 +0300

    new

--- src/Car.java
+++ src/Car.java
@@ -8,19 +8,19 @@ public class Car {
 
     public Car(String brand, String model, int productionYear, String country, String color, double engine) {
         if (brand == null){
-            this.brand = null; }
+            this.brand = "default"; }
         else { this.brand = brand; }
 
         if (model == null){
-            this.model = null; }
+            this.model = "default"; }
         else { this.model = model; }
 
-        if (productionYear == 0){
+        if (productionYear <= 0){
             this.productionYear = 2000; }
         else { this.productionYear = productionYear; }
 
         if (country == null){
-            this.country = null; }
+            this.country = "default"; }
         else { this.country = country; }
 
         if (color == null){
@@ -44,7 +44,7 @@ public class Car {
     public static void main(String[] args) {
         Car one = new Car(null, "Grande", 2015, "Россия", null, 0);
         Car two = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черный", 3.0);
-        Car three = new Car("BMW", "Z8", 2021, "Германии", "черный", 3.0);
+        Car three = new Car("BMW", "Z8", 2021, null, "черный", 3.0);
         Car four = new Car("Kia", "Sportage 4 поколение", 2018, "Южной Корее", "красный", 2.4);
         Car five = new Car("Hyundai", "Avante", 2016, "Южной Корее", "оранжевый", 1.6);
 
