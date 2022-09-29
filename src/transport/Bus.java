cf96f412cddb659c74e5ac2f9476baedfe4766d8
sergeydomanov <nomad-labs@yandex.ru>
2022-09-29 21:56:57 +0300

    added new homework

---
+++ src/transport/Bus.java
@@ -0,0 +1,8 @@
+package transport;
+
+public class Bus extends Transport {
+
+    public Bus(String brand, String model, int productionYear, String country, String color, double maxSpeed) {
+        super(brand, model, productionYear, country, color, maxSpeed);
+    }
+}
--- src/transport/Train.java
+++ src/transport/Train.java
@@ -1,4 +1,64 @@
 package transport;
 
-public class Train {
+public class Train extends Transport {
+    double cost;
+    double time;
+    String departure;
+    String arrival;
+    int quantityWagon;
+
+
+    public Train(String brand, String model, int productionYear, String country, String color, double maxSpeed, double cost) {
+        super(brand, model, productionYear, country, color, maxSpeed);
+
+        if (cost < 0) {
+            System.out.println("Укажите стоимость проезда");
+        } else {
+            this.cost = cost;
+        }
+
+        if (time < 0) {
+            System.out.println("Укажите время");
+        } else {
+            this.time = time;
+        }
+
+        if (departure == null || departure.isEmpty()) {
+            this.departure = "default";
+        } else {
+            this.departure = departure;
+        }
+
+        if (arrival == null || arrival.isEmpty()) {
+            this.arrival = "default";
+        } else {
+            this.arrival = arrival;
+        }
+
+        if (quantityWagon < 0) {
+            System.out.println("Укажите количество вагон");
+        } else {
+            this.quantityWagon = quantityWagon;
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public int getQuantityWagon() {
+        return quantityWagon;
+    }
 }
