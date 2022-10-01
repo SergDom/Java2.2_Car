acc2507faacad8e1c6bc9fbce72053bce8ddbf2f
sergeydomanov <nomad-labs@yandex.ru>
2022-10-01 12:28:49 +0300

    added new homework

---
+++ src/Animals/Zoo.java
@@ -0,0 +1,15 @@
+package Animals;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+
+        Predators giena = new Predators("Забияка", 10, "Савана", "Падаль");
+        giena.setSpeed(10);
+
+        System.out.println(giena);
+
+
+
+    }
+}
